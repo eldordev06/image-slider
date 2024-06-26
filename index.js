37e871e649c6737f88a092891c3258c41173fba5
Eldor <eldorforprogramming@gmail.com>
2024-06-26 17:01:47 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+let slides = document.querySelectorAll(".slide");
+let slideIndex = 0;
+let intervalId = null;
+
+document.addEventListener("DOMContentLoaded", initializeSlider);
+
+function initializeSlider() {
+    if (slides.length > 0) {
+        slides[slideIndex].classList.add("displaySlide");
+        intervalId = setInterval(nextSlide, 3000);
+    }
+}
+
+function showSlide(index) {
+    if (index >= slides.length) {
+        slideIndex = 0;
+    } else if (index < 0) {
+        slideIndex = slides.length - 1;
+    }
+
+    slides.forEach(el => el.classList.remove("displaySlide"));
+    slides[slideIndex].classList.add("displaySlide");
+}
+
+function nextSlide() {
+    slideIndex++;
+    showSlide(slideIndex);
+}
+
+function prevSlide() {
+    clearInterval(intervalId);
+    slideIndex--;
+    showSlide(slideIndex);
+}
